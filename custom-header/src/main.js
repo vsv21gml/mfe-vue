07c318d4e68074fd0e63f8e9cd772c5bbdc3cc26
heejun <vsv21gml@naver.com>
2023-04-16 15:43:08 +0900

    mfe test

---
+++ custom-header/src/main.js
@@ -0,0 +1,3 @@
+import { defineCustomElement } from "vue";
+import MainHeader from './MainHeader.vue'
+customElements.define("main-header", defineCustomElement(MainHeader));
---
+++ custom-header/vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // outputDir: '../app/public',
+  // filenameHashing: false,
+}
